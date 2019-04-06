22767c3dd655b77cae20e2b8fd619f4e66e43a54
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-06 15:23:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(i_say)
        return "I LOVE YOU TOO PUMPKIN!"
     elsif i_say == i_say.downcase
         return "HUH?! SPEAK UP, SONNY!"
-    else i_say == i_say.upcase
+    else 
         return "NO, NOT SINCE 1938!"
    end
 end
